f3aa35d76d41e25c092e462bac0ab44fc59e9434
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2022-08-19 15:22:51 -0300

    Apollo Provider and first query to test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,94 @@
-function App() {
+import { gql, useQuery } from '@apollo/client';
 
-  return (
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
-  )
+const GET_LESSONS_QUERY = gql`
+    fragment FileParts on UploadFileEntityResponse {
+        data {
+            id
+            attributes {
+                alternativeText
+                name
+                height
+                width
+                url
+                formats
+            }
+        }
+    }
+    query {
+        lessons {
+            data {
+                id
+                attributes {
+                    title
+                    slug
+                    description
+                    availableAt
+                    type
+                    challenge {
+                        data {
+                            id
+                            attributes {
+                                URL
+                            }
+                        }
+                    }
+                    teacher {
+                        data {
+                            id
+                            attributes {
+                                name
+                                bio
+                                avatar {
+                                    ...FileParts
+                                }
+                            }
+                        }
+                    }
+                    Video {
+                        __typename
+                        ... on ComponentVideoVideoFromLibrary {
+                            id
+                            title
+                            media {
+                                ...FileParts
+                            }
+                        }
+                        ... on ComponentVideoVideoFromYouTube {
+                            id
+                            videoID
+                        }
+                        ... on ComponentVideoVideoFromVimeo {
+                            id
+                            videoID
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+interface Lessons {
+    data: [lesson];
 }
 
-export default App
+interface lesson {
+    id: string;
+    attributes: {
+        title: string;
+    };
+}
+
+function App() {
+    const { data } = useQuery<{ lessons: Lessons }>(GET_LESSONS_QUERY);
+
+    return (
+        <ul>
+            {data?.lessons?.data.map(lesson => {
+                return <li key={lesson.id}>{lesson.attributes.title}</li>;
+            })}
+        </ul>
+    );
+}
+
+export default App;
