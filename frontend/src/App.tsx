9af3b79fa14a06b2addc5a68f3006a7f368f48a1
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2022-08-22 09:04:01 -0300

    Start componentes and pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,94 +1,88 @@
-import { gql, useQuery } from '@apollo/client';
+// import { gql, useQuery } from '@apollo/client';
 
-const GET_LESSONS_QUERY = gql`
-    fragment FileParts on UploadFileEntityResponse {
-        data {
-            id
-            attributes {
-                alternativeText
-                name
-                height
-                width
-                url
-                formats
-            }
-        }
-    }
-    query {
-        lessons {
-            data {
-                id
-                attributes {
-                    title
-                    slug
-                    description
-                    availableAt
-                    type
-                    challenge {
-                        data {
-                            id
-                            attributes {
-                                URL
-                            }
-                        }
-                    }
-                    teacher {
-                        data {
-                            id
-                            attributes {
-                                name
-                                bio
-                                avatar {
-                                    ...FileParts
-                                }
-                            }
-                        }
-                    }
-                    Video {
-                        __typename
-                        ... on ComponentVideoVideoFromLibrary {
-                            id
-                            title
-                            media {
-                                ...FileParts
-                            }
-                        }
-                        ... on ComponentVideoVideoFromYouTube {
-                            id
-                            videoID
-                        }
-                        ... on ComponentVideoVideoFromVimeo {
-                            id
-                            videoID
-                        }
-                    }
-                }
-            }
-        }
-    }
-`;
+// const GET_LESSONS_QUERY = gql`
+//     fragment FileParts on UploadFileEntityResponse {
+//         data {
+//             id
+//             attributes {
+//                 alternativeText
+//                 name
+//                 height
+//                 width
+//                 url
+//                 formats
+//             }
+//         }
+//     }
+//     query GetLessons {
+//         lessons {
+//             data {
+//                 id
+//                 attributes {
+//                     title
+//                     slug
+//                     description
+//                     availableAt
+//                     type
+//                     challenge {
+//                         data {
+//                             id
+//                             attributes {
+//                                 URL
+//                             }
+//                         }
+//                     }
+//                     teacher {
+//                         data {
+//                             id
+//                             attributes {
+//                                 name
+//                                 bio
+//                                 avatar {
+//                                     ...FileParts
+//                                 }
+//                             }
+//                         }
+//                     }
+//                     Video {
+//                         __typename
+//                         ... on ComponentVideoVideoFromLibrary {
+//                             id
+//                             title
+//                             media {
+//                                 ...FileParts
+//                             }
+//                         }
+//                         ... on ComponentVideoVideoFromYouTube {
+//                             id
+//                             videoID
+//                         }
+//                         ... on ComponentVideoVideoFromVimeo {
+//                             id
+//                             videoID
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// `;
+//
+// interface Lessons {
+//     data: [lesson];
+// }
+//
+// interface lesson {
+//     id: string;
+//     attributes: {
+//         title: string;
+//     };
+// }
 
-interface Lessons {
-    data: [lesson];
+import { Event } from './pages/Event';
+
+export function App() {
+    // const { data } = useQuery<{ lessons: Lessons }>(GET_LESSONS_QUERY);
+
+    return <Event />;
 }
-
-interface lesson {
-    id: string;
-    attributes: {
-        title: string;
-    };
-}
-
-function App() {
-    const { data } = useQuery<{ lessons: Lessons }>(GET_LESSONS_QUERY);
-
-    return (
-        <ul>
-            {data?.lessons?.data.map(lesson => {
-                return <li key={lesson.id}>{lesson.attributes.title}</li>;
-            })}
-        </ul>
-    );
-}
-
-export default App;
